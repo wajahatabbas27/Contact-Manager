4ebd7cb42b941f069df84615d5af2b6a887cf120
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-07-17 18:54:14 +0500

    auth api created both for post and for get

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+
+    //getting token from the header
+    const token = req.header('x-auth-token');
+
+    //if token doesnot exist
+    if (!token) {
+        return res.status(401).json({ msg: "Authorization denied, token missing" });
+    }
+
+    //verify token
+    try {
+
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();                                       //next leke ajaega user queue chala dega
+
+    } catch (err) {
+        res.status(401).json({ msg: "Invalid token" });
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,89 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const User = require('../model/User');
+const auth = require('../middleware/auth');
 
-// @route   GET api/auth
+
+
+// @route   GET api/auth 
 // @desc    Get the logged in user
 // @access  Private
-router.get('/', (req, res) => res.send("Get Logged in User"));
+router.get('/', auth,
+    async (req, res) => {
+        try {
+            const user = await User.findById(req.user.id).select('-password');                          //select se password return nhi hoga
+            res.json(user);
+        }
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    });
 
-// @route   POST api/auth
+
+
+
+// @route   POST api/auth - jb user login hoga to authentication krega yh
 // @desc    Authorize user and get the token
 // @access  Public
-router.post('/', (req, res) => res.send("Login the user"));
+router.post('/',
+    [
+        check('email', 'Please provide valid email').isEmail(),
+        check('password', 'Please enter a password').exists()
+    ],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        };
+
+        const { email, password } = req.body;
+
+        try {
+
+            //check kr rhe hain database mein hai ya nhi 
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ msg: "Register a user with this email" })
+            }
+
+            //hash password ko decode kreinge take authentication hojae hmare pass jo password enter hoa hai usse 
+            const isMatch = await bcrypt.compare(password, user.password);                                            // jo password req.body se aya hai usko compare kr rhe hain database wale user.password se 
+
+            //agr match nhi hoa to show krdega incorrect password
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Incorrect Password" })
+            }
+
+            //agr match hogae user aur password dono ,  to hm token send krrhe hain.
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'),
+                { expiresIn: 3600000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                })
+
+        }
+        catch (err) {
+
+            console.error(err.message);
+            res.status(500).send('Server Error');
+
+        }
+
+    });
 
 module.exports = router;
