b1ed30df3e00915e6f9e20ae5cd92ad1658e0d21
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-07-19 23:35:22 +0500

    completed contact route 2 apis get and post and also created the model

---
+++ model/Contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const contactSchema = mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    relationship: {
+        type: String,
+        default: 'Personal'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('contact', contactSchema);
